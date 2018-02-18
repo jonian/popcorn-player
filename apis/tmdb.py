0da61b95f8b9243d3efdb5304ea34ba22db43010
Jonian Guveli <jonian@hardpixel.eu>
2018-02-19 00:31:46 +0200

    add tmdb api class

---
+++ apis/tmdb.py
@@ -0,0 +1,26 @@
+import os
+
+from helpers.request import Request
+
+API_KEY = os.environ.get('TMDB_API_KEY')
+
+class Tmdb(Request):
+
+  BASE_URL = 'https://api.themoviedb.org/3'
+  PARAMS   = { 'api_key': API_KEY }
+
+  def find(self, imdb_id, **params):
+    params['external_source'] = 'imdb_id'
+    return self.get('find/%s' % imdb_id, params)
+
+  def movies(self, sort='popular', **params):
+    return self.get('movie/%s' % sort, params)
+
+  def movie(self, movie_id, **params):
+    return self.get('movie/%s' % movie_id, params)
+
+  def tvshows(self, sort='popular', **params):
+    return self.get('tv/%s' % sort, params)
+
+  def tvshow(self, show_id, **params):
+    return self.get('tv/%s' % show_id, params)
