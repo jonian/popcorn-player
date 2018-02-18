71b755247a906bf123b65f75812db62a26e4dcdb
Jonian Guveli <jonian@hardpixel.eu>
2018-02-18 17:39:49 +0200

    add yts api search support

--- apis/yts.py
+++ apis/yts.py
@@ -4,11 +4,15 @@ class Yts(Request):
 
   base_url = 'https://yts.am/api/v2'
 
+  def search(self, term, **params):
+    params['query_term'] = term
+    return self.movies(**params)
+
+  def upcoming(self):
+    return self.get('list_upcoming.json')
+
   def movies(self, **params):
     return self.get('list_movies.json', params)
-
-  def upcoming(self, **params):
-    return self.get('list_upcoming.json', params)
 
   def movie(self, movie_id, **params):
     params['movie_id'] = movie_id
